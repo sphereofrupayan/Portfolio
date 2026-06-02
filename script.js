cc1709c74dbcf9de255ab51e6f5bbfa1887adef6
Rupayan <rupayanchattaraj@gmail.com>
2026-06-02 12:23:20 +0530

    Add VANTA effects and improve canvas animations

--- script.js
+++ script.js
@@ -378,3 +378,18 @@ setInterval(() => {
     typeObserver.observe(resumeCard);
 
 })();
+
+
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+document.querySelectorAll('.hidden').forEach(el => {
+  observer.observe(el);
+});
